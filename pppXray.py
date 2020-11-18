25fbb5ab77c797ecc02fa916583e8180039b3a9a
潘一 <2585614464@qq.com>
2020-11-18 18:13:23 +0800

    add code

---
+++ pppXray.py
@@ -0,0 +1,68 @@
+import os
+import hashlib
+import re
+
+def logo():
+    logo='''
+ _ __  _ __  _ __         
+| '_ \| '_ \| '_ \        
+| |_) | |_) | |_) |       
+| .__/| .__/| .__/        
+| |   | |   | |           
+|_|   |_|   |_|           
+   __   __                
+   \ \ / /                
+    \ V / _ __ __ _ _   _ 
+    /   \| '__/ _` | | | |
+   / /^\ \ | | (_| | |_| |
+   \/   \/_|  \__,_|\__, |
+                     __/ |
+                    |___/ 
+                            v1.0
+                            author:springbird
+    '''
+    return logo
+
+
+def xrayScan(targeturl,outputfilename="test"):
+    scanCommand="xray.exe webscan --basic-crawler {} --html-output {}.html".format(targeturl,outputfilename)
+    print(scanCommand)
+    os.system(scanCommand)
+    return
+
+# def test():
+#     pattern = re.compile(r'^http://')
+#     m = pattern.match('http://www.baidu.com')
+#     n = pattern.match('hta')
+#     print(m)
+#     print(n)
+#     return
+
+def pppGet():
+    f = open("target.txt")
+    lines = f.readlines()
+    pattern = re.compile(r'^http://')
+    for line in lines:
+        try:
+            if not pattern.match(line.strip()):
+                targeturl="http://"+line.strip()
+            else:
+                targeturl=line.strip()
+            print(targeturl.strip())
+            outputfilename=hashlib.md5(targeturl.encode("utf-8"))
+            xrayScan(targeturl.strip(), outputfilename.hexdigest())
+            # print(type(line))
+        except Exception as e:
+            print(e)
+            pass
+    f.close()
+    print("Xray Scan End~")
+    return
+
+def main():
+    print(logo())
+    pppGet()
+    return
+
+if __name__ == '__main__':
+    main()
